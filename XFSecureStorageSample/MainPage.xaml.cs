b5f47e48d7c8d525dd6ab0733bc064ae3005e9fc
Gerald Versluis <gerald@verslu.is>
2021-10-07 12:29:34 +0200

    Implement secure storage

--- XFSecureStorageSample/MainPage.xaml.cs
+++ XFSecureStorageSample/MainPage.xaml.cs
@@ -14,5 +14,26 @@ namespace XFSecureStorageSample
         {
             InitializeComponent();
         }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Xamarin.Essentials.SecureStorage.SetAsync("password", password.Text);
+        }
+
+        async void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            string password = "";
+
+            try
+            {
+                password = await Xamarin.Essentials.SecureStorage.GetAsync("password");
+            }
+            catch
+            {
+                password = "nothing yet";
+            }
+
+            await DisplayAlert("Your Password is", password, "OK");
+        }
     }
 }
